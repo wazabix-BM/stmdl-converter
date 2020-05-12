7a3c07a850d31e86b810dbe66d16c542872d246b
WaZaBiX <rl.wazabix@gmail.com>
2020-05-12 15:56:27 +0200

    files

---
+++ index.js
@@ -0,0 +1,12 @@
+/* => ***
+* Imports and exports directories ***
+*/
+
+module.exports = {
+  convertM: require ('./lib/mass.js'),
+  convertD: require ('./lib/convertD'),
+  convertL: require ('./lib/length.js'),
+  convertS: require ('./lib/speed.js'),
+  convertT: require ('./lib/time.js')
+
+}
